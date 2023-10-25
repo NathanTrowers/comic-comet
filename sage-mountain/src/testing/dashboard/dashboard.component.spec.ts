96dda7aab303ff4d6c3fd147d4f7be6782c90ab2
NathanTrowers <53019476+NathanTrowers@users.noreply.github.com>
2023-10-25 14:23:30 +0000

    Updated the README (#1)
    also fixed a failing test

--- sage-mountain/src/testing/dashboard/dashboard.component.spec.ts
+++ sage-mountain/src/testing/dashboard/dashboard.component.spec.ts
@@ -2,14 +2,17 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from '../../app/dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let activatedRouteStub: Partial<ActivatedRoute>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule, DashboardComponent]
+      imports: [HttpClientModule, DashboardComponent],
+      providers: [ { provide: ActivatedRoute, useValue: activatedRouteStub }]
     });
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
