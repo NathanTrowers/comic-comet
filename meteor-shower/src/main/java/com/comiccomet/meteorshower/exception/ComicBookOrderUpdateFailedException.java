abe12c2b60572c6174cb9b96257d2f01ed497d1f
NathanTrowers <trownate@gmail.com>
2024-03-06 15:57:29 -0500

    PR CHANGES: formatting change in Java file

--- meteor-shower/src/main/java/com/comiccomet/meteorshower/exception/ComicBookOrderUpdateFailedException.java
+++ meteor-shower/src/main/java/com/comiccomet/meteorshower/exception/ComicBookOrderUpdateFailedException.java
@@ -4,5 +4,4 @@ public class ComicBookOrderUpdateFailedException extends RuntimeException {
     public ComicBookOrderUpdateFailedException(String orderId, String comicBookId) {
         super("Unable to update comic book order with orderId " + orderId  + " and comicBookId " + comicBookId);
     }
-    
 }
